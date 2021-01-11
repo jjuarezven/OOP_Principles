aba24cf8f34aea6fbbc7d8fdbad94827b445184e
Jose G. Juarez R <josejuarez@outlook.com>
2021-01-11 18:02:34 -0500

    Add project files.

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string EmailAddess { get; set; }
+        public string FirstName  { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                string fullName = LastName;
+                if (!string.IsNullOrWhiteSpace(FirstName))
+                {
+                    if (!string.IsNullOrWhiteSpace(LastName))
+                    {
+                        fullName += ", ";
+                    }
+                    fullName += FirstName;
+                }
+                return fullName;
+            }
+        }
+    }
+}
---
+++ Tests/ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,50 @@
+﻿using ACM.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            var customer = new Customer
+            {
+                FirstName = "Jose",
+                LastName = "Juarez"
+            };
+            string expected = "Juarez, Jose";
+            string actual = customer.FullName;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FullNameFirstNameEmpty()
+        {
+            var customer = new Customer
+            {
+                LastName = "Juarez"
+            };
+            string expected = "Juarez";
+            string actual = customer.FullName;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FullNameLastNameEmpty()
+        {
+            var customer = new Customer
+            {
+                FirstName = "Jose"
+            };
+            string expected = "Jose";
+            string actual = customer.FullName;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
